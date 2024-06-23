5abae60d452855275e613025d78f86b742b75b8a
Joshua Dierickse <joshua.dierickse@gmail.com>
2024-06-23 14:32:40 -0400

    Update

--- Linux-Init.sh
+++ Linux-Init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #File: Linux-Init.sh
 
-echo "hello world!"
+cd ~
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
+
+oh-my-posh init pwsh --config "~/.poshthemes/jblab_2021.omp.json" | Invoke-Expression
+
+echo "# oh-my-posh setup\neval \"$(oh-my-posh --init --shell bash --config ~/.poshthemes/jblab_2021.omp.json)\""
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Linux-Init
 Linux-Init
+
+
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/iWolf22/Linux-Init/HEAD/Linux-Init.sh)"
+```
